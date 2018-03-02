7b906744620e19c047782530d56ca5a2b2cb6e52
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-03-02 13:17:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
-const Discord = require("discord.js");
-const client = new Discord.Client;
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-client.on("message", (message) => {
-  if (message.content === ("ping")) {
-    message.replay("pong!");
-  }
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.login("NDE5MTMzOTEyMzMwMjcyNzc5.DXr_uQ.M3NgvCFyiIwmcPEpa1CmH4s4BR4")
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
